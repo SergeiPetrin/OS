c35ab1818cb8ea12b573c7c8ea3f9fa2f0611134
Petrinking <54847414+SergeiPetrin@users.noreply.github.com>
2019-11-22 19:29:49 +0300

    Create laba2.c

---
+++ laba2.c
@@ -0,0 +1,42 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <mcheck.h>
+#include <stdio.h>
+#include <math.h>
+
+
+
+
+void* threadFunc(void* args){
+    int *num = (int*) args;
+   
+// Короче pthread_create не видит мою функцию и я не знаю что делать тут должно вычисляться cos + sin.
+
+    return res;
+}
+
+int main() {
+    long double granica1;
+    long double granica2;
+    long double dlinaOtrezka;
+    int to4ki;
+    printf("Введите первую границу\n");
+    scanf("%Le", &granica1);
+    printf("Введите вторую границу\n");
+    scanf("%Le", &granica2);
+    printf("Введите количество точек\n");
+    scanf("%d", &to4ki);
+    pthread_t massivPotokov[to4ki];
+    long double dots[to4ki];
+    long double result[to4ki];
+    for(int i = 1; i <= to4ki; i++) {
+        dlinaOtrezka = granica2 - granica1;
+        dots[i] = (((dlinaOtrezka * i) / (to4ki - 1)) + granica1) - 1;
+        printf("Точка dots[i] = %Lf\n", dots[i]);
+        result[i] = pthread_create(&massivPotokov[i], NULL, threadFunc, &dots[i]);
+
+    }
+    for(int j = 0; j < to4ki; j++) {
+        printf("Результат в точке %Lf\n", result[j]);
+    }
+}
